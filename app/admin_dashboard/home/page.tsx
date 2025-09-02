99b56d4ca8ebd7590381f28371c5aa735645c71f
Maël KEMPF-LE PAPE <mael.kempf-le-pape@epitech.eu>
2025-09-02 13:03:50 +0200

    [ADD] dashboard admin and db connections

---
+++ app/admin_dashboard/home/page.tsx
@@ -0,0 +1,13 @@
+import Home from "../home_dashboard";
+import SideNavbar from "../components/SideNavbar";
+
+export default function AdminHomePage() {
+  return (
+    <div>
+      <SideNavbar />
+      <Home />
+    </div>
+  );
+}
+
+
---
+++ app/admin_dashboard/page.tsx
@@ -0,0 +1,13 @@
+import AdminDashboardPage from "./dashboard";
+import SideNavbar from "./components/SideNavbar";
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <SideNavbar />
+      <section id="admin_dashboard">
+        <AdminDashboardPage />
+      </section>
+    </div>
+  );
+}
