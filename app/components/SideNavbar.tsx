6dbe7095b833b02820dd7afba464c06986cde04a
Narkowz <kowlairenukzz@gmail.com>
2025-09-04 10:24:03 +0200

    remove: login button in navbar in multiple

--- app/components/SideNavbar.tsx
+++ app/components/SideNavbar.tsx
@@ -109,15 +109,6 @@ export default function SideNavbar() {
           ), 
           href: "/public_pages/pitch-deck" 
         },
-        { 
-          name: "Login", 
-          icon: (
-            <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-            </svg>
-          ),
-          href: "/public_pages/login"
-        },
         {
           name: "Messages",
           icon: (
