d622c346c633a11f4b262243305efe2bdfd27211
Maël KEMPF-LE PAPE <mael.kempf-le-pape@epitech.eu>
2025-09-10 16:28:45 +0200

    [ADD] center catalog page and remove black in news descrption

--- app/public_pages/catalog/page.tsx
+++ app/public_pages/catalog/page.tsx
@@ -8,21 +8,21 @@ export default function CatalogHome() {
       className="font-sans min-h-screen w-full"
       style={{
         background: `
-          linear-gradient(110deg, 
+          linear-gradient(110deg,
             #F18585 0%,
             #F49C9C 10%,
             #F6AEAE 26%,
             #F8CACF 48%,
             #EED5FB 72%,
-            #CB90F1 88%, 
+            #CB90F1 88%,
             #C174F2 100%)
         `,
       }}
     >
       <SideNavbar />
       <main
-        className="flex flex-col items-center justify-center min-h-[70vh] w-full py-16 ml-0 md:ml-56 transition-all duration-300"
-        style={{ marginLeft: "var(--side-navbar-width, 14rem)" }}
+        className="flex flex-col items-center justify-center min-h-[70vh] w-full py-16 transition-all duration-300"
+        style={{ paddingLeft: "var(--side-navbar-width, 14rem)" }}
       >
         <ProjectsCatalog />
       </main>
