d4024a73bd8cd2c059e717a75dbeed5031e1225e
Maël KEMPF-LE PAPE <mael.kempf-le-pape@epitech.eu>
2025-09-11 14:02:45 +0200

    [FIX] background profile page

--- app/public_pages/profile/page.tsx
+++ app/public_pages/profile/page.tsx
@@ -5,11 +5,17 @@ import ProfileEditor from "./ProfileEditor";
 
 export default function ProfilePage() {
   return (
-    <div className="min-h-screen bg-gray-900 dark:bg-gray-900 flex">
+    <div
+      className="font-sans min-h-screen w-full relative"
+      style={{
+        background:
+          "linear-gradient(180deg, #F18585 0%, #F49C9C 18%, #F6AEAE 34%, #F8CACF 50%, #EED5FB 56%, #E4BEF8 72%, #D5A8F2 85%, #CB90F1 94%, #C174F2 100%)",
+      }}
+    >
       <SideNavbar />
-      <main className="flex-1 overflow-auto px-4 py-8">
-        <ProfileEditor />
-      </main>
+      <ProfileEditor />
     </div>
   );
 }
+
+
